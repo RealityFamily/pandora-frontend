aaf5152efc5c912625bc378c7baae4968fc2c70e
artemmad <artemmakarevich1997@gmail.com>
2021-07-22 21:14:58 +0300

    Added all users displaying

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './UsersList.module.css';
-import {Button, Input, Layout, Table} from "antd";
+import {Button, Input, Layout, Table, Tag} from "antd";
 import {Header} from "antd/es/layout/layout";
 import {PlusOutlined} from "@ant-design/icons";
 import {Link} from "react-router-dom";
+import UserDataService from "../../service/api/UserDataService";
+
 
 const dataSource = [
     {
@@ -11,21 +13,21 @@ const dataSource = [
         id: "8a8260ec-cbb7-4ead-a2ae-7d4fa6c4ad96",
         mail: 'testuser@gmail.com',
         nickname: 'testuser',
-        role: 'User',
+        role: ['USER'],
     },
     {
         key: '2',
         id: "161980ca-e02b-4f7c-bab9-52d2807a6fc3",
         mail: 'admintest@gmail.com',
         nickname: 'admin',
-        role: 'Admin',
+        role: ['ADMIN'],
     },
     {
         key: '3',
         id: "62075f11-20cd-486a-b0ac-322fa840058f",
         mail: 'artemmakarevich1997@gmail.com',
         nickname: 'artemboomAss',
-        role: 'User',
+        role: ['USER'],
     },
 
 ];
@@ -50,17 +52,53 @@ const columns = [
         title: 'Роль',
         dataIndex: 'role',
         key: 'role',
+        render: tags => (
+            <>
+                {tags?.map(tag => {
+                    let color = 'geekblue';
+                    if (tag === 'USER') {
+                        color = 'green';
+                    }
+                    return (
+                        <Tag color={color} key={tag}>
+                            {tag.toUpperCase()}
+                        </Tag>
+                    );
+                })}
+            </>
+        )
     },
     {
         title: 'Действия',
         key: 'operation',
-        render: (_, record) => <Link to={`/admin/user/${record.id}`}>Редактировать</Link>},
+        render: (_, record) => <Link to={`/admin/user/${record?.id}`}>Редактировать</Link>},
 ];
 
 class UsersList extends React.Component {
 
+    state = {
+        userList: []
+    }
+
+    componentDidMount() {
+        UserDataService.retrieveAllUsers().then(resp =>{
+            console.log(resp.data);
+            this.setState({
+                userList: resp.data?.map( user => {
+                    let newUser= {};
+                    newUser.key = user.id;
+                    newUser.id = user.id;
+                    newUser.mail = user.mail;
+                    newUser.nickname = user.nickname;
+                    newUser.role = user.role;
+                    return newUser;
+                })
+            })
+        })
+    }
 
     render() {
+
         return <div className={styles.UsersList}>
             <Layout>
                 <Header theme={"light"} style={{
@@ -77,7 +115,7 @@ class UsersList extends React.Component {
                     </Button>
                 </Header>
 
-                <Table dataSource={dataSource} columns={columns}/>
+                <Table dataSource={this.state.userList} columns={columns}/>
 
             </Layout>
         </div>
